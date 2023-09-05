2fa3c869453076002c7b5f534082df0ac7a14d4a
bacho1927 <bacho.karashvili@gmail.com>
2023-09-05 13:17:36 +0400

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,28 @@
 import './Contact.css'
 import Reveal from './Reveal'
 import React from 'react';
-import {Link} from 'react-scroll'
-export default function Contact(){
-    const {elementRef , shouldAnimate} = Reveal()
+import { Link } from 'react-scroll'
+export default function Contact() {
+    const { elementRef, shouldAnimate } = Reveal()
 
-    return(
+    return (
         <>
             <div className='main-contact-container' >
-                <h1 className={`contact-header ${shouldAnimate ? 'reveal-side' : ''}`} ref={elementRef} style={{animationDelay:'1000ms'}}>Contact</h1>
-                <div className={`contact-container ${shouldAnimate ? 'reveal-up' : ''}`} style={{animationDelay:'2000ms'}} >
+                <h1 className={`contact-header ${shouldAnimate ? 'reveal-side' : ''}`} ref={elementRef} style={{ animationDelay: '1000ms' }}>Contact</h1>
+                <div className={`contact-container ${shouldAnimate ? 'reveal-up' : ''}`} style={{ animationDelay: '2000ms' }} >
                     <h2 >Would you like to work with me?</h2>
                     <a className='contact' href="mailto:Bacho.karashvili@gmail.com">Email: Bacho.Karashvili@gmail.com</a>
-                    
-                    
+                    <a className='contact' href="https://www.linkedin.com/in/bacho-karashvili-90b3b1269/">LinkedIn: linkedin.com/in/bacho-karashvili</a>
+
                 </div>
                 <Link
-                    
-                    to="hero" 
+
+                    to="hero"
                     smooth={true}
-                    duration={500} 
-                    ><div className={`top ${shouldAnimate ? 'reveal-up' : ''}`} style={{animationDelay: '2500ms'}}>^</div></Link>
+                    duration={500}
+                ><div className={`top ${shouldAnimate ? 'reveal-up' : ''}`} style={{ animationDelay: '2500ms' }}>^</div></Link>
             </div>
-        
+
         </>
     )
 }
